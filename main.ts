22f5ac8f6e5773312f9c605bf7fd3f3db49dea28
Nick Rosier <github@bunbun.be>
2020-12-30 17:23:48 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+basic.forever(function () {
+    music.ringTone(262)
+    basic.pause(100)
+    music.ringTone(330)
+    basic.pause(100)
+    music.ringTone(392)
+    basic.pause(100)
+    music.ringTone(330)
+    basic.pause(100)
+})
